15d48d9c0096eb48c923a1adc1b9a65caa598a12
Pablo Recio Quijano <precio@zocolab.es>
2011-12-16 23:49:15 +0800

    Fixing style

--- fivesquares/foursquareapi/utils.py
+++ fivesquares/foursquareapi/utils.py
@@ -1,37 +1,48 @@
 import urllib2
 import json
 
+
 def get_data(url):
     open = urllib2.urlopen(url)
     line = open.readlines()[0]
     data = json.loads(line)
     return data['response']['venues']
 
+
 def show_data(mylist, indent=1, level=0):
     for item in mylist:
-        if isinstance(item,list):
-            show_data(item,indent,level+1)
+        if isinstance(item, list):
+            show_data(item, indent, level + 1)
         else:
             if indent:
                 for tab in range(level):
                     print "\t"
             print item
 
-def order_data(lista):
-  ordena_quicksort(lista,0,len(lista)-1)
-  return lista
 
-def ordena_quicksort(lista,izdo,dcho):
-  if izdo<dcho:
-    pivote=lista[(izdo+dcho)/2]['location']['distance']
-    i,d=izdo,dcho
-    while i<=d :
-      while lista[i]['location']['distance']<pivote : i+=1
-      while lista[d]['location']['distance']>pivote : d-=1
-      if i<=d :
-        lista[i]['location']['distance'],lista[d]['location']['distance']=lista[d]['location']['distance'],lista[i]['location']['distance']
-        i+=1
-        d-=1
-    if izdo<d : ordena_quicksort(lista,izdo,d)
-    if i<dcho : ordena_quicksort(lista,i,dcho)
-  return lista
+def order_data(elements):
+    ordena_quicksort(elements, 0, len(elements) - 1)
+    return elements
+
+
+def ordena_quicksort(elements, left, right):
+    if left < right:
+        pivot = elements[(left + right) / 2]['location']['distance']
+        l, r = left, right
+        while l <= r:
+            while elements[l]['location']['distance'] < pivot:
+                l += 1
+            while elements[r]['location']['distance'] > pivot:
+                r -= 1
+            if l <= r:
+                (elements[l]['location']['distance'],
+                 elements[r]['location']['distance']) = (
+                     elements[r]['location']['distance'],
+                     elements[l]['location']['distance'])
+                l += 1
+                r -= 1
+        if left < r:
+            ordena_quicksort(elements, left, r)
+        if l < right:
+            ordena_quicksort(elements, l, right)
+    return elements
