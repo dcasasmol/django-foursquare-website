1722f2a96e46c15a384a47c40d8370be9b9f1232
Pablo Recio Quijano <precio@zocolab.es>
2011-12-16 14:20:24 +0100

    Added basic forms.py that simple validates the location given

---
+++ fivesquares/foursquareapi/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+
+class BasicQueryForm(forms.Form):
+    position = forms.CharField()
+
+    def clean():
+        cleaned_data = self.cleaned_data
+        ll = cleaned_data['position'].replace(' ', '').split(',')
+
+        if len(ll) != 2 or not ll[0].isdigit() or not ll[1].isdigit():
+            raise forms.ValidationError(
+                _('The given position value is not valid'))
+        return cleaned_data
